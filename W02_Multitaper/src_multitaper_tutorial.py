a9ebd8166df67d5adeeb088205fc8f4359c7541a
arash <A.Shahidi@campus.lmu.de>
2025-10-30 16:28:17 +0100

    [DATALAD] Recorded changes

--- W02_Multitaper/src_multitaper_tutorial.py
+++ W02_Multitaper/src_multitaper_tutorial.py
@@ -1,7 +1,25 @@
 from pathlib import Path
+import scipy.io
+import ghostipy as gsp
+
 datadir = Path("/storage2/arash/teaching/neuropy/data/ds-montgomery-v1")
 filename = datadir / "ws_data_1shank.mat"
 
-import scipy.io
 data = scipy.io.loadmat(filename)
 print(data.keys())
+
+lfp = data["lfps"].squeeze()  # shape (n_channels, n_times)
+print(lfp.shape)
+
+fs = 1250  # Sampling frequency in Hz
+channel = 0  # Select the first channel
+signal = lfp[channel, :]
+
+# Parameters for multitaper spectral estimation
+bandwidth = 4.0  # Time-bandwidth product
+nfft = 2048          # Number of FFT points
+# ... adjust parameters as needed
+
+# run spectral estimation
+
+# explore and do the exercise!
